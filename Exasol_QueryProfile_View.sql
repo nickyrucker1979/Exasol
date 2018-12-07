1559198b050adf2f00c68202d07858c373659926
Nicky Rucker <nickyrucker@hotmail.com>
2018-12-07 11:17:54 -0700

    Update Exasol_QueryProfile_View.sql

--- Exasol_QueryProfile_View.sql
+++ Exasol_QueryProfile_View.sql
@@ -1,3 +1,19 @@
+----  option 1  -----
+alter session set profile = 'ON';
+set autocommit off;
+-- write query to profile
+
+select * from sales where sales_id = 1;
+
+commit;
+flush statistics;  -- triggers the log server to write the profile or it will only run once a minute
+alter session set profile = 'OFF';
+
+select * from exa_user_profile_last_day
+where session_id=current_session;
+
+
+-----  option 2  -----
 -- write query below - example....
 select
   count(*)
@@ -5,7 +21,7 @@ from
   table1 t1
   join table2 t2
     on t1.id = t2.id
-where 
+where
   td.person_id = 10;
 
 -- view the query profile
